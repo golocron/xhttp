f12bf8c6b5b242afa9f8e302017a08a279fea6a2
PavelBrm <pavelbrm@disroot.org>
2019-05-07 20:28:04 +1200

    Add more tests and Makefile

--- xhttp_test.go
+++ xhttp_test.go
@@ -1,10 +1,19 @@
 package xhttp
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+type testRequest struct {
+	code int
+	mtd  string
+	msg  string
+	body []byte
+	err  error
+}
 
 func createTestHandler(code int, msg string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -14,20 +23,15 @@ func createTestHandler(code int, msg string) http.HandlerFunc {
 }
 
 func TestGet(t *testing.T) {
-	type testItem struct {
-		code int
-		msg  string
-	}
-
 	type testCase struct {
-		expected *testItem
+		expected *testRequest
 		actual   *Response
 	}
 
 	tests := []*testCase{
-		{expected: &testItem{200, "success"}},
-		{expected: &testItem{400, "bad request"}},
-		{expected: &testItem{500, "internal server error"}},
+		{expected: &testRequest{code: 200, msg: "success"}},
+		{expected: &testRequest{code: 400, msg: "bad request"}},
+		{expected: &testRequest{code: 500, msg: "internal server error"}},
 	}
 
 	for _, tc := range tests {
@@ -39,6 +43,106 @@ func TestGet(t *testing.T) {
 			if err != nil {
 				srv.Close()
 				tt.Errorf("failed to GET: %s", err)
+			}
+			srv.Close()
+
+			if tc.actual.StatusCode != tc.expected.code {
+				tt.Errorf("expected resp code %d, actual is %d", tc.expected.code, tc.actual.StatusCode)
+			}
+		})
+	}
+}
+
+func TestSend(t *testing.T) {
+	type testCase struct {
+		expected *testRequest
+		actual   *Response
+	}
+
+	tests := []*testCase{
+		{expected: &testRequest{code: 200, mtd: http.MethodGet, msg: "success"}},
+		{expected: &testRequest{code: 400, mtd: http.MethodGet, msg: "bad request"}},
+		{expected: &testRequest{code: 500, mtd: http.MethodGet, msg: "internal server error"}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.expected.msg, func(tt *testing.T) {
+			srv := httptest.NewServer(http.Handler(createTestHandler(tc.expected.code, tc.expected.msg)))
+
+			req := NewRequest(tc.expected.mtd, srv.URL, tc.expected.body)
+
+			var err error
+			tc.actual, err = Send(req)
+			if err != nil {
+				srv.Close()
+				tt.Errorf("failed to Send: %s", err)
+			}
+			srv.Close()
+
+			if tc.actual.StatusCode != tc.expected.code {
+				tt.Errorf("expected resp code %d, actual is %d", tc.expected.code, tc.actual.StatusCode)
+			}
+		})
+	}
+}
+
+func TestSendRaw(t *testing.T) {
+	type testCase struct {
+		expected *testRequest
+		actual   *http.Response
+	}
+
+	tests := []*testCase{
+		{expected: &testRequest{code: 200, mtd: http.MethodGet, msg: "success"}},
+		{expected: &testRequest{code: 400, mtd: http.MethodGet, msg: "bad request"}},
+		{expected: &testRequest{code: 500, mtd: http.MethodGet, msg: "internal server error"}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.expected.msg, func(tt *testing.T) {
+			srv := httptest.NewServer(http.Handler(createTestHandler(tc.expected.code, tc.expected.msg)))
+
+			req, err := http.NewRequest(tc.expected.mtd, srv.URL, bytes.NewBuffer(tc.expected.body))
+			if err != nil {
+				srv.Close()
+				tt.Errorf("failed to Send: %s", err)
+			}
+
+			tc.actual, err = SendRaw(req)
+			if err != nil {
+				srv.Close()
+				tt.Errorf("failed to Send: %s", err)
+			}
+			srv.Close()
+
+			if tc.actual.StatusCode != tc.expected.code {
+				tt.Errorf("expected resp code %d, actual is %d", tc.expected.code, tc.actual.StatusCode)
+			}
+		})
+	}
+}
+
+func TestPost(t *testing.T) {
+	type testCase struct {
+		expected *testRequest
+		actual   *Response
+	}
+
+	tests := []*testCase{
+		{expected: &testRequest{code: 200, mtd: http.MethodPost, msg: "success", body: []byte("success")}},
+		{expected: &testRequest{code: 400, mtd: http.MethodPost, msg: "bad request", body: []byte("bad request")}},
+		{expected: &testRequest{code: 500, mtd: http.MethodPost, msg: "internal server error", body: []byte("internal server error")}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.expected.msg, func(tt *testing.T) {
+			srv := httptest.NewServer(http.Handler(createTestHandler(tc.expected.code, tc.expected.msg)))
+
+			var err error
+			tc.actual, err = Post(srv.URL, "application/octet-stream", tc.expected.body)
+			if err != nil {
+				srv.Close()
+				tt.Errorf("failed to Send: %s", err)
 			}
 			srv.Close()
 
